1496b46313f765f0bfa6de4281ea7abc2535683f
Jeef-Jr <jefferson.goncalvesfo@sptech.school>
2022-06-14 21:18:46 -0300

    Adicionado icon cart e nome de retorno de página

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import { TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,7 +21,19 @@ function Routes() {
         <Stack.Screen
           name="Detail"
           component={Detail}
-          options={{ title: "Awesome app" }}
+          options={{
+            headerBackTitle: "Home",
+            headerRight: () => (
+              <TouchableOpacity>
+                <Feather
+                  style={{ marginRight: 15 }}
+                  name="shopping-cart"
+                  size={24}
+                  color="white"
+                />
+              </TouchableOpacity>
+            ),
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
